5f3c1eda211c3a45527d0382f687db5229e937dd
millim <lidian@yiqijiao.cn>
2019-11-26 10:26:33 +0800

    添加tags

---
+++ public/javascripts/app.js
@@ -0,0 +1,22 @@
+function getQueryString(name) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == name) { return pair[1]; }
+  }
+  return (false);
+}
+
+
+function getShowObj(curTag){
+  return function isOnlyShow(name) {
+    if (curTag == null) {
+      return $("#group-" + name).show()
+    }
+    if (curTag == name) {
+
+      $("#group-" + name).show()
+    }
+  }
+}
